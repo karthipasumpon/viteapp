ee7ba1249a30930675de9b1b564eafa54e075e53
akashnewport <shaliniragu53@gmail.com>
2025-03-07 09:53:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+function App() {
+  return (
+    <div>
+      <h1>Main Heading</h1>
+      <Header />
+    </div>
+  )
+}
+
+export default App
